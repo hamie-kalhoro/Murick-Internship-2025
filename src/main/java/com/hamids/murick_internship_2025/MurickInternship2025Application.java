90a1bc7a297d08d4ad4570168aca57a7d67aa944
Hamid Ali Khan <aleehamid1024@gmail.com>
2025-09-21 23:44:53 +0500

    adding initial files

---
+++ src/main/java/com/hamids/murick_internship_2025/MurickInternship2025Application.java
@@ -0,0 +1,13 @@
+package com.hamids.murick_internship_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurickInternship2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MurickInternship2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamids/murick_internship_2025/MurickInternship2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamids.murick_internship_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurickInternship2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
